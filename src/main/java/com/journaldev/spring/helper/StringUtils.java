73630b45fda43bf4f961e0afc36276b5d6cb6dac
rishiThakur <bhoopthakur@hotmail.com>
2019-03-26 12:20:39 +1100

    stringutils commit

--- src/main/java/com/journaldev/spring/helper/StringUtils.java
+++ src/main/java/com/journaldev/spring/helper/StringUtils.java
@@ -1,6 +1,8 @@
 package com.journaldev.spring.helper;
 
 import java.security.SecureRandom;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import javax.crypto.Cipher;
 import javax.crypto.SecretKey;
@@ -11,7 +13,26 @@ import org.apache.commons.codec.binary.Base64;
 import org.apache.commons.lang.RandomStringUtils;
 
 public class StringUtils {
+	
+	public static final String EMAIL_ADDRESS = "^((([A-Za-z]|\\d|[!#\\$%&\\'\\*\\+\\-\\/=\\?\\^_`{\\|}~]|[\\u00A0-\\uD7FF\\uF900-\\uFDCF\\uFDF0-\\uFFEF])+(\\.([A-Za-z]|\\d|[!#\\$%&\\'\\*\\+\\-\\/=\\?\\^_`{\\|}~]|[\\u00A0-\\uD7FF\\uF900-\\uFDCF\\uFDF0-\\uFFEF])+)*)|((\\x22)((((\\x20|\\x09)*(\\x0d\\x0a))?(\\x20|\\x09)+)?(([\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x7f]|\\x21|[\\x23-\\x5b]|[\\x5d-\\x7e]|[\\u00A0-\\uD7FF\\uF900-\\uFDCF\\uFDF0-\\uFFEF])|(\\\\([\\x01-\\x09\\x0b\\x0c\\x0d-\\x7f]|[\\u00A0-\\uD7FF\\uF900-\\uFDCF\\uFDF0-\\uFFEF]))))*(((\\x20|\\x09)*(\\x0d\\x0a))?(\\x20|\\x09)+)?(\\x22)))@((([A-Za-z]|\\d|[\\u00A0-\\uD7FF\\uF900-\\uFDCF\\uFDF0-\\uFFEF])|(([A-Za-z]|\\d|[\\u00A0-\\uD7FF\\uF900-\\uFDCF\\uFDF0-\\uFFEF])([A-Za-z]|\\d|-|\\.|_|~|[\\u00A0-\\uD7FF\\uF900-\\uFDCF\\uFDF0-\\uFFEF])*([A-Za-z]|\\d|[\\u00A0-\\uD7FF\\uF900-\\uFDCF\\uFDF0-\\uFFEF])))\\.)+(([A-Za-z]{2}|[\\u00A0-\\uD7FF\uF900-\\uFDCF\\uFDF0-\\uFFEF])|(([A-Za-z]|[\\u00A0-\\uD7FF\\uF900-\\uFDCF\\uFDF0-\\uFFEF])([A-Za-z]|\\d|-|\\.|_|~|[\\u00A0-\\uD7FF\\uF900-\\uFDCF\\uFDF0-\\uFFEF])*([A-Za-z]|[\\u00A0-\\uD7FF\\uF900-\\uFDCF\\uFDF0-\\uFFEF])))\\.?$";
 
+
+	
+	private boolean validEmail(String emailArray) {
+		boolean validEmail= true;
+		String[] emails = emailArray.split(",");
+
+		Pattern pattern = Pattern.compile(EMAIL_ADDRESS, Pattern.CASE_INSENSITIVE);
+		for (String email : emails) {
+			Matcher matcher = pattern.matcher(email.trim());
+			if (!matcher.matches()) {
+				validEmail= false;
+				break;
+			}
+		}
+		return validEmail;
+
+	}
 	private String generateRandomNumber(final char firstCharacter, final char secondCharacter) {
 		final StringBuffer trn = new StringBuffer();
 		trn.append(firstCharacter);
